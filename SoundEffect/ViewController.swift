2f3c417d480b5500e9343f9af235496f72d1d7d5
labnova <progettofeb@gmail.com>
2014-12-21 14:56:28 +0100

    Initial Commit

---
+++ SoundEffect/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SoundEffect
+//
+//  Created by Innocenzo Tremamondo on 21/12/14.
+//  Copyright (c) 2014 LabNova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
